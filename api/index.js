a761ec805f4828a7bf7651d8df49dbc5cc33aca4
RammY254 <luckymutisya83@gmail.com>
2026-08-03 15:47:57 +0300

    fix: Remove stale dbInitializedPromise named import from api/index.js to fix Vercel serverless function syntax error

--- api/index.js
+++ api/index.js
@@ -1,12 +1,6 @@
-import app, { dbInitializedPromise } from '../server/index.js';
+import app from '../server/index.js';
 
-export default async function handler(req, res) {
-  if (dbInitializedPromise) {
-    try {
-      await dbInitializedPromise;
-    } catch (err) {
-      console.error('Database connection error in Vercel function:', err);
-    }
-  }
+export default function handler(req, res) {
   return app(req, res);
 }
+
